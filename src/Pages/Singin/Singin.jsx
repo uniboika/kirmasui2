d90ee29d165ab7963a6edbdcc346ade3c04d722a
Nazif Abdullahi <nazifabdullahi003@gmail.com>
2024-03-07 10:27:02 +0100

    nochanges

--- src/Pages/Singin/Singin.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Singin() {
-  return (
-    <div>
-      <header>hello</header>
-    </div>
-  )
-}
--- src/Pages/Singin/Singup.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Singup() {
-  return (
-    <div>
-      Singup
-    </div>
-  )
-}
